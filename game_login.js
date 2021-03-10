b85325ed39366fbb8d0f23156da6e13666d2c6fb
mohit <75508470+Mohit-Coder99@users.noreply.github.com>
2021-03-09 19:40:16 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,17 @@
+function addUser() {
+
+    window.location.replace("game_page.html");
+
+
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("Player1_name" , player1_name);
+    localStorage.setItem("Player2_name" , player2_name);
+
+
+    
+
+
+
+}
